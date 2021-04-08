d2c2cbd3fdb06ebe420e01b9ccdff01a4b36677d
thenatural86 <philip.smalls86@gmail.com>
2021-04-07 22:24:34 -0400

    custom hook for data fetching

--- src/components/CustomHookComponent.tsx
+++ src/components/CustomHookComponent.tsx
@@ -1,3 +1,31 @@
+import { useState, useEffect } from 'react'
+
+export interface Beverage {
+  name: string
+  brand: string
+}
+
+function UseFetchData<Payload>(
+  url: string
+): {
+  data: Payload | null
+  done: boolean
+} {
+  const [data, setData] = useState<Payload | null>(null)
+  const [done, setDone] = useState<boolean>(false)
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((d: Payload) => {
+        setData(d)
+        setDone(true)
+      })
+  }, [url])
+  return { data, done }
+}
+
 export default function CustomHookComponent() {
-  return <h5>'Customize'</h5>
+  const { data, done } = UseFetchData<Beverage[]>('/og.json')
+  return <div>{done && data![0].name}</div>
 }
