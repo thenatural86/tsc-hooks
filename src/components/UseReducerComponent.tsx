20a5825e7373500a522741e5cf335b281cd642b9
thenatural86 <philip.smalls86@gmail.com>
2021-04-07 21:27:03 -0400

    usereducer with typescript

--- src/components/UseReducerComponent.tsx
+++ src/components/UseReducerComponent.tsx
@@ -1,7 +1,39 @@
+import { useReducer } from 'react'
+
+const initialState = {
+  counter: 100,
+}
+
+type ActionTypes =
+  | { type: 'increment'; payload: number }
+  | { type: 'decrement'; payload: number }
+
+function counterReducer(state: typeof initialState, action: ActionTypes) {
+  switch (action.type) {
+    case 'increment':
+      return { ...state, counter: state.counter + action.payload }
+
+    case 'decrement':
+      return { ...state, counter: state.counter - action.payload }
+
+    default:
+      throw new Error('bad action')
+  }
+}
+
 export default function UseReducerComponent() {
+  const [state, dispatch] = useReducer(counterReducer, initialState)
+
   return (
     <div>
-      <h4>"reduce"</h4>
+      <div>{state.counter}</div>
+      <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>
+        Increment
+      </button>{' '}
+      |
+      <button onClick={() => dispatch({ type: 'decrement', payload: 7 })}>
+        Decrement
+      </button>
     </div>
   )
 }
